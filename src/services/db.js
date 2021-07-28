92b1ded9b0b5ebfdcb31e597969d1b5ead8110e2
Dean Spooner <dean.spooner@me.com>
2021-07-28 19:27:46 +0100

    Prepare for Heroku.

--- src/services/db.js
+++ src/services/db.js
@@ -1,15 +1,24 @@
 const mysql = require('mysql2/promise');
 
-const { DB_PASSWORD, DB_USER, DB_NAME, DB_HOST, DB_PORT } = process.env;
+const {
+  DB_PASSWORD,
+  DB_USER,
+  DB_NAME,
+  DB_HOST,
+  DB_PORT,
+  CLEARDB_DATABASE_URL,
+} = process.env;
 
 module.exports = async () => {
-  const connection = await mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    database: DB_NAME,
-  });
+  const connection = CLEARDB_DATABASE_URL
+    ? await mysql.createConnection(CLEARDB_DATABASE_URL)
+    : await mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        port: DB_PORT,
+        database: DB_NAME,
+      });
 
   return connection;
 };
--- utils/create-database.js
+++ utils/create-database.js
@@ -10,19 +10,31 @@ require('dotenv').config({
   path: path.join(__dirname, envFile),
 });
 
-const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
+const {
+  DB_PASSWORD,
+  DB_NAME,
+  DB_USER,
+  DB_HOST,
+  DB_PORT,
+  CLEARDB_DATABASE_URL,
+} = process.env;
 
 const setUpDatabase = async () => {
   try {
-    const db = await mysql.createConnection({
-      host: DB_HOST,
-      user: DB_USER,
-      password: DB_PASSWORD,
-      port: DB_PORT,
-    });
+    const db = CLEARDB_DATABASE_URL
+      ? await mysql.createConnection(CLEARDB_DATABASE_URL)
+      : await mysql.createConnection({
+          host: DB_HOST,
+          user: DB_USER,
+          password: DB_PASSWORD,
+          port: DB_PORT,
+          database: DB_NAME,
+        });
 
-    await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
-    await db.query(`USE ${DB_NAME}`);
+    // create the database if it doesn't already exist
+    !CLEARDB_DATABASE_URL &&
+      (await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`));
+    !CLEARDB_DATABASE_URL && (await db.query(`USE ${DB_NAME}`));
     await db.query(`CREATE TABLE IF NOT EXISTS child(
             id INT PRIMARY KEY auto_increment,
             childName VARCHAR(25),
