c54b3ae0f35d06288636ebfa8a005b9fe8737cce
Dean Spooner <dean.spooner@me.com>
2021-08-02 22:40:52 +0100

    Added columns for schoolClass and userType.

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -2,13 +2,13 @@ const getDb = require('../services/db');
 
 exports.create = async (req, res) => {
   const db = await getDb();
-  const { username, password } = req.body;
+  const { username, password, schoolClass, userType } = req.body;
 
   try {
-    await db.query('INSERT INTO users (username, password) VALUES (?, ?)', [
-      username,
-      password,
-    ]);
+    await db.query(
+      'INSERT INTO users (username, password, schoolClass, userType) VALUES (?, ?)',
+      [username, password, schoolClass, userType]
+    );
     res.sendStatus(201);
   } catch (err) {
     res.sendStatus(500).json(err);
