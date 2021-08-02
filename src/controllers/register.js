05543569ff1f6cbf51946bafad316a935c82f012
Dean Spooner <dean.spooner@me.com>
2021-08-02 19:31:38 +0100

    Added basic user registration functionality.

---
+++ src/controllers/register.js
@@ -0,0 +1,18 @@
+const getDb = require('../services/db');
+
+exports.register = async (req, res) => {
+  const db = await getDb();
+  const { username, password } = req.body;
+
+  try {
+    await db.query('INSERT INTO users (username, password) VALUES (?, ?)', [
+      username,
+      password,
+    ]);
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500).json(err);
+    console.log(err);
+  }
+  db.close();
+};
---
+++ src/routes/register.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const registerController = require('../controllers/register');
+
+const router = express.Router();
+
+router.post('/register', registerController.register);
+
+module.exports = router;
