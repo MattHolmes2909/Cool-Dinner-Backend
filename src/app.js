e75310ddd4b647682e05d2ddb48955d2cb1a1408
Dean Spooner <dean.spooner@me.com>
2021-08-02 19:53:10 +0100

    Added CORS.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const childRouter = require('./routes/child');
 const canteenRouter = require('./routes/canteen');
 const registerRouter = require('./routes/register');
@@ -7,12 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.use(cors());
 
 app.use('/child', childRouter);
 
