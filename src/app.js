dc55186c0a178399d31c747de1f3069a514afd41
Dean Spooner <dean.spooner@me.com>
2021-08-07 16:13:30 +0100

    CORS again.

--- src/app.js
+++ src/app.js
@@ -7,6 +7,15 @@ const loginRouter = require('./routes/login');
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.use('/child', childRouter);
