eb66961ff2929bcf3f10a5f21bda2b2101061267
Dean Spooner <dean.spooner@me.com>
2021-08-07 18:07:42 +0100

    Further rollback.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const canteenRouter = require('./routes/canteen');
 const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
 
-const app = express();
+const app = express(express.json());
 
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
@@ -12,8 +12,6 @@ app.use((req, res, next) => {
   res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-
-app.use(express.json());
 
 app.use('/child', childRouter);
 
