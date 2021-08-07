88f2f6a01d5daa5f5f9a791c6addf9c218870bf6
Dean Spooner <dean.spooner@me.com>
2021-08-07 16:10:02 +0100

    CORS.

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,7 +4,7 @@ const loginController = require('../controllers/login');
 const router = express.Router();
 
 router.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
