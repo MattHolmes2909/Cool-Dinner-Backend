d2196bee83e2c63b04070091395fc98c623f0ccf
Dean Spooner <dean.spooner@me.com>
2021-08-08 13:17:31 +0100

    More ACAH fields added.

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ const app = express(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
   next();
 });
 
