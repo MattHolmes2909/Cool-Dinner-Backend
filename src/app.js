7ba0696d78b4f8c7df1929476c5c92e4c0cba617
Dean Spooner <dean.spooner@me.com>
2021-08-08 16:27:31 +0100

    Moved CORS and json.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const childRouter = require('./routes/child');
 const canteenRouter = require('./routes/canteen');
 // const registerRouter = require('./routes/register');
 // const loginRouter = require('./routes/login');
 
-const app = express(express.json());
+const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  next();
-});
+app.use(cors());
+
+app.use(express.json());
 
 app.use('/child', childRouter);
 
