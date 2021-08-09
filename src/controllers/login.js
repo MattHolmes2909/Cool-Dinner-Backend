f0aa95c3175b8d197037e3a6327e45b9c74c89aa
Dean Spooner <dean.spooner@me.com>
2021-08-09 17:01:14 +0100

    First stage of adding JWT auth.

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,5 +1,23 @@
 const getDb = require('../services/db');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// const verifyJWT = (req, res, next) => {
+//   const token = req.headers['x-access-token'];
+
+//   if (!token) {
+//     res.send('No token provided.');
+//   } else {
+//     jwt.verify(token, 'jwtSecret', (err, decoded) => {
+//       if (err) {
+//         res.json({ auth: false, message: 'Failed to authenticate.' });
+//       } else {
+//         req.userId = decoded.id;
+//         next();
+//       }
+//     });
+//   }
+// };
 
 exports.login = async (req, res) => {
   const db = await getDb();
@@ -11,18 +29,24 @@ exports.login = async (req, res) => {
     ]);
 
     if (row.length === 0) {
-      res.send('Invalid username.').sendStatus(500);
+      res.json({auth: false, message: "Invalid username."}.sendStatus(500);
     }
 
     const checkPass = await bcrypt.compare(password, row[0].password);
 
     if (checkPass === false) {
-      res.send('Password is incorrect.').sendStatus(500);
+      res.json({auth: false, message: "Wrong password."}.sendStatus(500);
     } else {
-      res.send('Login successful.').sendStatus(201);
+      const id = row[0].id;
+      const token = jwt.sign({ id }, 'jwtSecret', {
+        expiresIn: 3600,
+      });
+      req.session.user = res; //jwtSecret would normally be replaced with dotenv var.
+      res.json({ auth: true, token: token, result: res }).sendStatus(201);
     }
   } catch (err) {
     res.sendStatus(500).json(err);
+    res.json({auth: false, message: "Server issues."})
     console.log(err);
   }
   db.close();
